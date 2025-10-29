27bb7233e570f3cb3f47a7400cdc4ff5553c55ac
cruciffix <mr.unnona@gmail.com>
2025-10-29 17:58:26 +0300

    страница услуг вид с компа

--- src/app/pages/uslugi/page.jsx
+++ src/app/pages/uslugi/page.jsx
@@ -9,20 +9,200 @@ export default function Uslugi() {
                 <div className="uslugiCol1">
 
                     <div className="uslugiShalgbaumAndVideo">
-                        <div className="uslugiShalgbaumAndVideoHeadlineWrapper">
-                            <span className="uslugiShalgbaumAndVideoHeadline">Надежная граница въезда/выезда</span>
+
+                        <div className="uslugiShalgbaumAndVideoTextContent">
+                            <div className="uslugiShalgbaumAndVideoHeadlineWrapper">
+                                <span className="uslugiShalgbaumAndVideoHeadline">Надежная граница въезда/выезда</span>
+                            </div>
+
+                            <div className="uslugiShalgbaumAndVideoDescWrapper">
+                                <p className="uslugiShalgbaumAndVideoDesc">
+                                    Прочный шлагбаум с автоматикой, гарантирующий стабильную работу и удобство всегда
+                                </p>
+                            </div>
+
+                            <div className="uslugiShalgbaumAndVideoListWrapper">
+                                <div className="uslugiShalgbaumAndVideoListItem">
+                                    <img src="/images/uslugi/uslugiParking.svg" className="uslugiShalgbaumAndVideoListItemImg"/>
+                                    <span className="uslugiShalgbaumAndVideoListItemText">Быстрый пропуск личных автомобилей</span>
+                                </div>
+                                
+                                <div className="uslugiShalgbaumAndVideoListItem">
+                                    <img src="/images/uslugi/uslugiHome.svg" className="uslugiShalgbaumAndVideoListItemImg"/>
+                                    <span className="uslugiShalgbaumAndVideoListItemText">Ограничение посторонних машин</span>
+                                </div>
+
+                                <div className="uslugiShalgbaumAndVideoListItem">
+                                    <img src="/images/uslugi/uslugiPredpriyatie.svg" className="uslugiShalgbaumAndVideoListItemImg"/>
+                                    <span className="uslugiShalgbaumAndVideoListItemText">Защита складских и производственных зон</span>
+                                </div>
+                            </div>
+
+                            
+                            <div className="uslugiShalgbaumAndVideoLinkWrapper">
+                                <a className="uslugiShalgbaumAndVideoLink" href="https://www.argos-kzn.ru/uslugi/avtomatizaciya-parkovok/">
+                                    Узнать далее
+                                </a>
+                            </div>
                         </div>
 
-                        <div className="uslugiShalgbaumAndVideoDescWrapper">
-                            <span className="uslugiShalgbaumAndVideoDesc">
-                                Прочный шлагбаум с автоматикой, гарантирующий стабильную работу и удобство всегда
-                            </span>
+                        <div className="uslugiShalgbaumAndVideoImgContent">
+                            <div className="uslugiShalgbaumAndVideoImgWrapper">
+                                <img className="uslugiShalgbaumAndVideoImg" src="/images/uslugi/uslugiShlag.png"/>
+                            </div>
                         </div>
 
-                        <div></div>
+
                     </div>
 
                 </div>
+
+                <div className="uslugiCol2">
+
+                    <div className="uslugiShalgbaumAndVideo">
+
+                        <div className="uslugiShalgbaumAndVideoTextContent">
+                            <div className="uslugiShalgbaumAndVideoHeadlineWrapper">
+                                <span className="uslugiShalgbaumAndVideoHeadline">Наблюдение за всем вашим объектом</span>
+                            </div>
+
+                            <div className="uslugiShalgbaumAndVideoDescWrapper">
+                                <p className="uslugiShalgbaumAndVideoDesc">
+                                    Наблюдение круглосуточно, запись на сервер, удалённый просмотр с телефона – удобно
+                                </p>
+                            </div>
+
+                            <div className="uslugiShalgbaumAndVideoListWrapper">
+                                <div className="uslugiShalgbaumAndVideoListItem">
+                                    <img src="/images/uslugi/uslugiHome.svg" className="uslugiShalgbaumAndVideoListItemImg"/>
+                                    <span className="uslugiShalgbaumAndVideoListItemText">Детекция подозрительного движения ночью</span>
+                                </div>
+                                
+                                <div className="uslugiShalgbaumAndVideoListItem">
+                                    <img src="/images/uslugi/uslugiSklad.svg" className="uslugiShalgbaumAndVideoListItemImg"/>
+                                    <span className="uslugiShalgbaumAndVideoListItemText">Снижение краж и потерь со склада</span>
+                                </div>
+
+                                <div className="uslugiShalgbaumAndVideoListItem">
+                                    <img src="/images/uslugi/uslugiPredpriyatie.svg" className="uslugiShalgbaumAndVideoListItemImg"/>
+                                    <span className="uslugiShalgbaumAndVideoListItemText">Аналитика поведения на предприятии</span>
+                                </div>
+                            </div>
+
+                            
+                            <div className="uslugiShalgbaumAndVideoLinkWrapper">
+                                <a className="uslugiShalgbaumAndVideoLink" href="https://www.argos-kzn.ru/uslugi/videonablyudenie/">
+                                    Подробнее
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="uslugiShalgbaumAndVideoImgContent">
+                            <div className="uslugiShalgbaumAndVideoImgWrapper">
+                                <img className="uslugiShalgbaumAndVideoImg" src="/images/uslugi/uslugiVideo.png"/>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+
+                <div className="uslugiCol3">
+                    <div className="uslugiAnotherWrapper">
+                        <div className="uslugiAnother">
+                            <div className="uslugiAnotherTextContent">
+                                <div className="uslugiAnotherHeadlineWrapper">
+                                    <span className="uslugiAnotherHeadline">Голосовой и видео контроль</span>
+                                </div>
+
+                                <div className="uslugiAnotherDescWrapper">
+                                    <p className="uslugiAnotherDesc">
+                                        Аудиосвязь, управление замком и простая эксплуатация
+                                    </p>
+                                </div>
+
+
+                                <div className="uslugiAnotherLinkWrapper">
+                                    <a className="uslugiAnotherLink" href="https://www.argos-kzn.ru/uslugi/domofon/">
+                                        Ознакомиться
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="uslugiAnotherImgContent">
+                                <div className="uslugiAnotherImgWrapper">
+                                    <img className="uslugiAnotherImg" src="/images/uslugi/uslugiDomofon.png"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="uslugiAnotherWrapper">
+                        <div className="uslugiAnother">
+                            <div className="uslugiAnotherTextContent">
+                                <div className="uslugiAnotherHeadlineWrapper">
+                                    <span className="uslugiAnotherHeadline">СКУД: электронная проходная</span>
+                                </div>
+
+                                <div className="uslugiAnotherDescWrapper">
+                                    <p className="uslugiAnotherDesc">
+                                        Турникеты интегрируемые с картами, биометрией и контроллерами
+                                    </p>
+                                </div>
+
+
+                                <div className="uslugiAnotherLinkWrapper">
+                                    <a className="uslugiAnotherLink" href="https://www.argos-kzn.ru/uslugi/SKUD-sistemi-kontrolya-i-ypravleniya-dostypom/">
+                                        Перейти
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="uslugiAnotherImgContent">
+                                <div className="uslugiAnotherImgWrapper">
+                                    <img className="uslugiAnotherImg" src="/images/uslugi/uslugiSKUD.png"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="uslugiAnotherWrapper">
+                        <div className="uslugiAnother">
+                            <div className="uslugiAnotherTextContent">
+                                <div className="uslugiAnotherHeadlineWrapper">
+                                    <span className="uslugiAnotherHeadline">Все типы ворот</span>
+                                </div>
+
+                                <div className="uslugiAnotherDescWrapper">
+                                    <p className="uslugiAnotherDesc">
+                                        Монтаж, настройка приводов и гарантийное обслуживание
+                                    </p>
+                                </div>
+
+
+                                <div className="uslugiAnotherLinkWrapper">
+                                    <a className="uslugiAnotherLink" href="https://www.argos-kzn.ru/uslugi/vorota/">
+                                        Узнать
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="uslugiAnotherImgContent">
+                                <div className="uslugiAnotherImgWrapper">
+                                    <img className="uslugiAnotherImg" src="/images/uslugi/uslugiVorota.png"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                 
+                </div>
+
+
+
+
 
             </div>
         </div>

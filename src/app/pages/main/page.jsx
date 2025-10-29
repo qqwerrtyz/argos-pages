67deda0832d93d38ca4d2dd6389d5cbb91fbcbc7
Alex <mr.unnona@gmail.com>
2025-10-29 11:33:50 +0300

    начал делать услуги

--- src/app/pages/main/page.jsx
+++ src/app/pages/main/page.jsx
@@ -2,35 +2,35 @@ import "./main.css"
 export default function Main() {
     return (
         <>
-            <div className="mainTitleWrapper">
-                <div className="mainTitle">
-                    <div className="mainTitleCol1">
-                        <div className="titleAndSubtitle">
-                            <h1 className="mainTitleTitle">
-                                Сложные системы безопасности с простым <span className="mainTitleTitleLittlePath">человеческим отношением</span>
+            <div class="mainTitleWrapper">
+                <div class="mainTitle">
+                    <div class="mainTitleCol1">
+                        <div class="titleAndSubtitle">
+                            <h1 class="mainTitleTitle">
+                                Сложные системы безопасности с простым <span class="mainTitleTitleLittlePath">человеческим отношением</span>
                             </h1>
-                            <p className="mainTitleSubtitle">Решаем вашу задачу на 100%, потому что для нас это дело принципа, а не просто работа. Гарантия до 5 лет</p>
+                            <p class="mainTitleSubtitle">Решаем вашу задачу на 100%, потому что для нас это дело принципа, а не просто работа. Гарантия до 5 лет</p>
                         </div>
 
-                        <div className="titleMainImageWrapper">
-                            <img className="titleMainImage" src="/images/main/title.jpg"/>
+                        <div class="titleMainImageWrapper">
+                            <img class="titleMainImage" src="/images/main/title.jpg"/>
                         </div>
                     </div>
 
-                    <div className="mainTitleCol2">
+                    <div class="mainTitleCol2">
 
-                        <div className="mainTitleFeedbackWrapper">
-                            <div className="mainTitleTgHeadlineWrapper">
-                                <span className="mainTitleTgHeadline">Напишите свой вопрос и получите список всех решений</span>
+                        <div class="mainTitleFeedbackWrapper">
+                            <div class="mainTitleTgHeadlineWrapper">
+                                <span class="mainTitleTgHeadline">Напишите свой вопрос и получите список всех решений</span>
                             </div>
                             
-                            <div className="mainTitleQrTgWrapper">
-                                <div className="mainTitleQrImageWrapper">
-                                    <a href="https://t.me/ArgosContact"><img className="mainTitleQrImage" src="/images/another/qrTG.jpg"/></a>
+                            <div class="mainTitleQrTgWrapper">
+                                <div class="mainTitleQrImageWrapper">
+                                    <a href="https://t.me/ArgosContact"><img class="mainTitleQrImage" src="/images/another/qrTG.jpg"/></a>
                                 </div>
 
-                                <a className="mainTitleLinkToTg" href="https://t.me/ArgosContact">
-                                    <img className="mainTitleIconTg" src="/images/another/tgYellow.svg"/> Написать в телеграм
+                                <a class="mainTitleLinkToTg" href="https://t.me/ArgosContact">
+                                    <img class="mainTitleIconTg" src="/images/another/tgYellow.svg"/> Написать в телеграм
                                 </a>
                             </div>
                         </div>
@@ -39,69 +39,162 @@ export default function Main() {
                 </div>
             </div>
             
-            <div className="whyRecomendedWrppaer">
-                <div className="whyRecomended">
+            <div class="whyRecomendedWrppaer">
+                <div class="whyRecomended">
 
-                    <div className="whyRecomendedHeadlineWrapper">
-                        <span className="whyRecomendedHeadline">Почему 9 из 10 клиентов рекомендуют нас своим партнерам?</span>
+                    <div class="uslugiHeadlineWrapper">
+                        <span class="uslugiHeadline">Почему 9 из 10 клиентов рекомендуют нас своим партнерам?</span>
                     </div>
 
-                    <div className="whyRecomendedItems">
+                    <div class="whyRecomendedItems">
 
-                        <div className="whyRecomendedItem">
-                            <div className="whyRecomendedItemHeadlineWrapper">
-                                <span className="whyRecomendedItemUnderHeadline">Экспертиза изнутри</span>
-                                <span className="whyRecomendedtemHeadline">Основатель – практик с 14-летним опытом</span>
+                        <div class="whyRecomendedItem">
+                            <div class="whyRecomendedItemTextContent">
+                                <div class="whyRecomendedItemHeadlineWrapper">
+                                    <span class="whyRecomendedItemUpperHeadline">Экспертиза изнутри</span>
+                                    <span class="whyRecomendedtemHeadline">Основатель – практик с 14-летним опытом</span>
+                                </div>
+
+                                <div class="whyRecomendedtemDescWrapper">
+                                    <p class="whyRecomendedtemDesc">
+                                        Наш директор сам прошел путь от монтажника до руководителя. Он и вся наша команда глубоко понимают каждый винтик в системе. Вы получаете не просто «услугу», а решение от людей, которые знают все технические нюансы
+                                    </p>
+                                </div>
                             </div>
-
-                            <div className="whyRecomendedtemDescWrapper">
-                                <p className="whyRecomendedtemDesc">
-                                    Наш директор сам прошел путь от монтажника до руководителя. Он и вся наша команда глубоко понимают каждый винтик в системе. Вы получаете не просто «услугу», а решение от людей, которые знают все технические нюансы
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="whyRecomendedItem">
-                            <div className="whyRecomendedItemHeadlineWrapper">
-                                <span className="whyRecomendedItemUnderHeadline">Работаем по совести</span>
-                                <span className="whyRecomendedtemHeadline">Честность и ответственность - наш кодекс</span>
-                            </div>
-
-                            <div className="whyRecomendedtemDescWrapper">
-                                <p className="whyRecomendedtemDesc">
-                                    Для нас нет «мелких» договоренностей. Мы честны в сметах, пунктуальны в сроках и несем полную ответственность за каждый проект. Ваше спокойствие - наша репутация
-                                </p>
+                            <div class="whyRecomendedItemImgWrapper">
+                                <img class="whyRecomendedItemImg" src="/images/main/expertiza.svg"/>
                             </div>
                         </div>
 
-                        <div className="whyRecomendedItem">
-                            <div className="whyRecomendedItemHeadlineWrapper">
-                                <span className="whyRecomendedItemUnderHeadline">Гибкость и инициатива</span>
-                                <span className="whyRecomendedtemHeadline">Ищем решение, а не отговорки</span>
+                        <div class="whyRecomendedItem">
+                            <div class="whyRecomendedItemTextContent">
+                                <div class="whyRecomendedItemHeadlineWrapper">
+                                    <span class="whyRecomendedItemUpperHeadline">Работаем по совести</span>
+                                    <span class="whyRecomendedtemHeadline">Честность и ответственность - наш кодекс</span>
+                                </div>
+
+                                <div class="whyRecomendedtemDescWrapper">
+                                    <p class="whyRecomendedtemDesc">
+                                        Для нас нет «мелких» договоренностей. Мы честны в сметах, пунктуальны в сроках и несем полную ответственность за каждый проект. Ваше спокойствие - наша репутация
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="whyRecomendedItemImgWrapper">
+                                <img class="whyRecomendedItemImg" src="/images/main/sovest.svg"/>
                             </div>
 
-                            <div className="whyRecomendedtemDescWrapper">
-                                <p className="whyRecomendedtemDesc">
-                                    Стандартные задачи решают все. Мы же беремся за сложное и нестандартное. Наша команда готова жертвовать личным временем, чтобы найти выход и соблюсти ваши интересы
-                                </p>
+                        </div>
+
+                        <div class="whyRecomendedItem">
+                            <div class="whyRecomendedItemTextContent">
+                                <div class="whyRecomendedItemHeadlineWrapper">
+                                    <span class="whyRecomendedItemUpperHeadline">Гибкость и инициатива</span>
+                                    <span class="whyRecomendedtemHeadline">Ищем решение, а не отговорки</span>
+                                </div>
+
+                                <div class="whyRecomendedtemDescWrapper">
+                                    <p class="whyRecomendedtemDesc">
+                                        Стандартные задачи решают все. Мы же беремся за сложное и нестандартное. Наша команда готова жертвовать личным временем, чтобы найти выход и соблюсти ваши интересы
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="whyRecomendedItemImgWrapper">
+                                <img class="whyRecomendedItemImg" src="/images/main/gibcost.svg"/>
                             </div>
                         </div>
 
-                        <div className="whyRecomendedItem">
-                            <div className="whyRecomendedItemHeadlineWrapper">
-                                <span className="whyRecomendedItemUnderHeadline">Гибкость и инициатива</span>
-                                <span className="whyRecomendedtemHeadline">Ищем решение, а не отговорки</span>
-                            </div>
+                        <div class="whyRecomendedItem">
+                            <div class="whyRecomendedItemTextContent">
+                                <div class="whyRecomendedItemHeadlineWrapper">
+                                    <span class="whyRecomendedItemUpperHeadline">Гибкость и инициатива</span>
+                                    <span class="whyRecomendedtemHeadline">Ищем решение, а не отговорки</span>
+                                </div>
 
-                            <div className="whyRecomendedtemDescWrapper">
-                                <p className="whyRecomendedtemDesc">
-                                    Стандартные задачи решают все. Мы же беремся за сложное и нестандартное. Наша команда готова жертвовать личным временем, чтобы найти выход и соблюсти ваши интересы
-                                </p>
+                                <div class="whyRecomendedtemDescWrapper">
+                                    <p class="whyRecomendedtemDesc">
+                                        Стандартные задачи решают все. Мы же беремся за сложное и нестандартное. Наша команда готова жертвовать личным временем, чтобы найти выход и соблюсти ваши интересы
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="whyRecomendedItemImgWrapper">
+                                <img class="whyRecomendedItemImg" src="/images/main/klush.svg"/>
                             </div>
                         </div>
 
                     </div>
 
+
+                </div>
+            </div>
+
+            <div class="mainServicesWrapper">
+                <div class="mainServices">
+
+                    <div class="mainServicesCol mainServicesCol1">
+                        <div class="mainServicesCallMeWrapper">
+                            <p class="mainServicesCallMe">Вы можете позвонить нам. Трубку возьмет самый компетентный сотрудник!</p>
+                            <a class="mainServicesLink" href="tel:+78432597991">
+                                <img src="/images/another/phoneWhite.svg" class="mainServicesLinkImg"/> Позвонить
+                            </a>
+                        </div>
+
+                        <div className="mainServicesAnotherServicesWrapper">
+                            <div className="mainServicesAnotherServicesRow">
+                                <div className="mainServicesAnotherServiceSkud">
+
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    <div class="mainServicesCol mainServicesCol2">
+                        <div class="mainServicesItem">
+                            <div class="mainServicesItemImgWrapper">
+                                <a href="https://www.argos-kzn.ru/uslugi/videonablyudenie/">
+                                    <img class="mainServicesItemImg" src="/images/main/vidoeMainService.jpg"/>
+                                </a>
+                            </div>
+
+                            <div class="mainServicesItemContent mainServicesItemContentVideonab">
+                                <div class="mainServicesItemTextContent">
+                                    <div class="mainServicesItemHeadlineWrapper">
+                                        <span class="mainServicesItemHeadline">Видеонаблюдение</span>
+                                    </div>
+
+                                    <p class="mainServicesItemDesc">
+                                        Установим камеры, которые действительно решают задачи: от контроля порядка в подъезде до защиты периметра склада. Поможем выбрать оборудование, которое не подведет
+                                    </p>
+                                </div>
+
+                                <a class="mainServicesItemLink" href="https://www.argos-kzn.ru/uslugi/videonablyudenie/">Узнать больше <img src="/images/another/arrowGo.svg" class="mainServicesItemArrow"/></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mainServicesCol mainServicesCol3">
+                        <div class="mainServicesItem">
+                            <div class="mainServicesItemImgWrapper">
+                                <a href="https://www.argos-kzn.ru/uslugi/avtomatizaciya-parkovok/">
+                                    <img class="mainServicesItemImg" src="/images/main/shlagMainService.jpg"/>
+                                </a>
+                            </div>
+
+                            <div class="mainServicesItemContent mainServicesItemContentSlagbaum">
+                                <div class="mainServicesItemTextContent">
+                                    <div class="mainServicesItemHeadlineWrapper">
+                                        <span class="mainServicesItemHeadline">Шлагбаумы</span>
+                                    </div>
+
+                                    <p class="mainServicesItemDesc">
+                                        Надежные решения для парковок, коттеджных поселков и частных территорий. Обеспечиваем бесперебойную работу и простоту использования
+                                    </p>
+                                </div>
+
+                                <a class="mainServicesItemLink" href="https://www.argos-kzn.ru/uslugi/avtomatizaciya-parkovok/">Перейти <img src="/images/another/arrowGo.svg" class="mainServicesItemArrow"/></a>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
--- src/app/pages/uslugi/page.jsx
+++ src/app/pages/uslugi/page.jsx
@@ -3,6 +3,28 @@ import "./uslugi.css";
 
 export default function Uslugi() {
     return (
-        <h1>kdnvkdvn</h1>
+        <div className="uslugiWrapper">
+            <div className="uslugi">
+
+                <div className="uslugiCol1">
+
+                    <div className="uslugiShalgbaumAndVideo">
+                        <div className="uslugiShalgbaumAndVideoHeadlineWrapper">
+                            <span className="uslugiShalgbaumAndVideoHeadline">Надежная граница въезда/выезда</span>
+                        </div>
+
+                        <div className="uslugiShalgbaumAndVideoDescWrapper">
+                            <span className="uslugiShalgbaumAndVideoDesc">
+                                Прочный шлагбаум с автоматикой, гарантирующий стабильную работу и удобство всегда
+                            </span>
+                        </div>
+
+                        <div></div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
     )
 }
